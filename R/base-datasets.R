a666ac3d24a0e4b9f1281cbe83a38b2af4d88198
abdala9512 <miguel.arquez12@gmail.com>
2020-12-27 23:33:12 -0500

    Documentation added

--- R/base-datasets.R
+++ R/base-datasets.R
@@ -1,4 +1,42 @@
+#' Call iris dataset
+#'
+#' Ideal dataset for clustering, multi-class
+#' problems (tree based models) and EDA.
+#'
+#' @return \code{iris} dataset with the following columns:
+#' \itemize{
+#'   \item Sepal.Length
+#'   \item Sepal.Width
+#'   \item Petal.Length
+#'   \item Petal.Width
+#'   \item Species
+#' }
+#' @export
+#'
+#' @author Miguel Arquez
+#'
+#' @examples
+#' my_iris_df <- getIris()
 getIris <-
 function(){
+  irisDF <- iris
+  cat("Classic Iris dataset imported.")
+  return(irisDF)
+}
+
+
+#' Load Iris dataset in the current environment
+#'
+#' Load the iris dataset as a \code{data.frame} object called
+#' \code{irisDF}
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' loadIris()
+loadIris <-
+function(){
   irisDF <<- iris
+  cat("Classic Iris dataset loaded.")
 }
---
+++ R/dareML.R
@@ -0,0 +1,9 @@
+#' dareML: Library for easily obtain clean and real datasets for machine learning projects or practive, suitable for differente types of algorithms.
+#'
+#'
+#'
+#'
+#' @author Miguel Arquez \email{miguel.arquez12@gmail.com}
+#' @docType package
+#' @name dareML
+"_PACKAGE"
