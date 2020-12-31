4926ee692d872fffc0c7ab149ca92da60eb7830c
abdala9512 <miguel.arquez12@gmail.com>
2020-12-31 11:52:03 -0500

    finance datasets

---
+++ R/finance-datasets.R
@@ -0,0 +1,15 @@
+
+
+#' Title
+#'
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+getVIX <-
+function(...){
+  #VIX daily from 2004
+  vix <- read.csv("https://datahub.io/core/finance-vix/r/vix-daily.csv")
+}
