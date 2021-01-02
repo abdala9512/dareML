24b2a9521a4976806c5e6f6efe3d2254b55b51b1
abdala9512 <miguel.arquez12@gmail.com>
2021-01-01 20:23:44 -0500

    Vix dataset loading

--- R/finance-datasets.R
+++ R/finance-datasets.R
@@ -13,3 +13,18 @@ function(...){
   #VIX daily from 2004
   vix <- read.csv("https://datahub.io/core/finance-vix/r/vix-daily.csv")
 }
+
+
+#' Title
+#'
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+loadVIX <-
+function(...){
+  vix <<- read.csv("https://datahub.io/core/finance-vix/r/vix-daily.csv")
+  message("VIX dataset loaded in the current environment.")
+}
