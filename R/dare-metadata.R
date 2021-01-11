1826ecce60a38b787b2b25b5017ab4bbe986e437
abdala9512 <miguel.arquez12@gmail.com>
2021-01-10 19:39:19 -0500

    dareML metadata functions

---
+++ R/dare-metadata.R
@@ -0,0 +1,12 @@
+
+dataSources <-
+function(){
+  message("--------------------------------------")
+  message("Current Data Sources - dateML library")
+  message("--------------------------------------")
+  message("* Datahub.io ")
+  message("* World bank")
+  message("* Kaggle ")
+
+  message("--------------------------------------")
+}
