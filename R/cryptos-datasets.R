3da8e040ca168bd448089feeeb699de074d2c308
abdala9512 <miguel.arquez12@gmail.com>
2021-04-25 22:34:53 -0500

    Crypto currencies API requests development

---
+++ R/cryptos-datasets.R
@@ -0,0 +1,16 @@
+#
+
+
+source("utils.R", encoding = "UTF-8")
+
+#' Title
+#'
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+getBitcoin <- function(...){
+
+}
--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,34 @@
 
+library(httr)
+library(tidyr)
+library(jsonlite)
 
 calculateSize <-
 function(obj){
   sprintf("Total size %s Mb", object.size(obj) / 1000000)
 }
+
+
+
+# Coin Market Cap - API  --------------------------------------------------
+
+getCryptoData <- function(API_KEY){
+
+  # https://towardsdatascience.com/top-5-best-cryptocurrency-apis-for-developers-32475d2eb749
+  # https://cran.r-project.org/web/packages/httr/vignettes/quickstart.html
+  # https://medium.com/@traffordDataLab/querying-apis-in-r-39029b73d5f1
+
+  BASE_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/ohlcv/historical'
+
+  request <- GET(url = BASE_URL,
+                 query = list(start = '1',
+                              limit = '5000',
+                              convert = 'USD'),
+                 config = add_headers(
+                   "Accepts" = "application/json",
+                   "X-CMC_PRO_API_KEY" =  API_KEY
+                   )
+                 )
+
+}
+
