ab4e8fc32223c195d5be655b07db6fb49562831e
Miguel Arquez Abdala <42754537+abdala9512@users.noreply.github.com>
2021-07-08 21:49:01 -0500

    Update cryptos-datasets.R

--- R/cryptos-datasets.R
+++ R/cryptos-datasets.R
@@ -1,6 +1,6 @@
 #
 
-gecko_base_url <- function() {"https://api.coingecko.com/api/v3"}
+gecko__api_base_url <- function() {"https://api.coingecko.com/api/v3"}
 
 #' getBitcoin data function
 #'
@@ -11,6 +11,6 @@ gecko_base_url <- function() {"https://api.coingecko.com/api/v3"}
 #'
 #' @examples
 getBitcoin <- function(...){
-  currentPrice <- fromJSON(paste0(gecko_base_url(), "/simple/price?ids=bitcoin&vs_currencies=usd"))
+  currentPrice <- fromJSON(paste0(gecko_api_base_url(), "/simple/price?ids=bitcoin&vs_currencies=usd"))
   return(currentPrice$bitcoin$usd)
 }
